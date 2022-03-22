4a452976ddcaa81a216e048af081250396400cab
Chinmayee-Kulkarni <101919148+Chinmayee-Kulkarni@users.noreply.github.com>
2022-03-22 21:04:31 +0530

    Delete background_Repository.java

--- hire/src/main/java/com/example/demo/dao/background_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entities.background_sync;
-
-public interface background_Repository extends CrudRepository<background_sync,String>{
-
-}
